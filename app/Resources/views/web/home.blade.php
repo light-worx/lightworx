fa05248a8ac2349aa17e3e249426da9a0df97bfe
Michael Bishop <michael@bishop.net.za>
2026-01-23 11:37:46 +0200

    Change byline

--- app/Resources/views/web/home.blade.php
+++ app/Resources/views/web/home.blade.php
@@ -1,7 +1,7 @@
 <x-lightworx::layouts.web pageName="Home">
     <div class="text-center">
         <img class="pt-5 img-fluid" src="{{asset('/lightworx/images/lightworx.png')}}">
-        <h3>Open source systems and solutions</h3>
+        <h3>Open source solutions</h3>
         <h5 class="py-5"><a style="text-decoration:none;background-color:#f9e258;color:black;" class="btn" href="mailto:{{setting('email_address')}}">Contact us</a></h5>
         <br><br><br>
     </div>
