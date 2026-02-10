8c589710abfd66747ecee6b6c5f4a641197a009c
Michael Bishop <michael@bishop.net.za>
2026-02-10 13:56:00 +0200

    Remove shield

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Althinect\FilamentSpatieRolesPermissions\Concerns\HasSuperAdmin;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
